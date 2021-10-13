81df90fbb5bc50fae1414dec7d781c2a0d9d8afe
yurrig <yu_gershanov@hotmail.com>
2021-10-12 23:20:22 -0500

    Lookup executables on the system path

--- hooks/hook.js
+++ hooks/hook.js
@@ -1,4 +1,3 @@
-
 function startDebugSession(type, block = true) {
     var process = require('process');
     var queueName = process.env['DEEPDEBUGGER_LAUNCHER_QUEUE'];
@@ -6,14 +5,31 @@ function startDebugSession(type, block = true) {
         var net = require('net');
         var sender = new net.Socket();
         sender.connect(queueName, function() {
+            var fs = require('fs');
             var path = require('path');
-            var hookPath = process.argv[2];
+            var programPath = process.argv[2];
+            if (!path.isAbsolute(programPath)) {
+                programPath = path.join(process.cwd(), programPath);
+                if (!fs.existsSync(programPath)) {
+                    var dirName = path.dirname(process.argv[2]);
+                    if (dirName === '.') {
+                        var splitPath = process.env['PATH'].split(process.platform === 'win32' ? ';' : ':');
+                        for (var pathPart in splitPath) {
+                            var testPath = path.join(splitPath[pathPart], process.argv[2]);
+                            if (fs.existsSync(testPath)) {
+                                programPath = testPath;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
             var msg = {
-                name: path.basename(hookPath),
+                name: path.basename(programPath),
                 type: type,
                 request: "launch",
                 cwd: process.cwd(),
-                program: path.isAbsolute(hookPath) ? hookPath : path.join(process.cwd(), hookPath),
+                program: programPath,
                 console: "integratedTerminal",
                 args: process.argv.slice(3)
             };
