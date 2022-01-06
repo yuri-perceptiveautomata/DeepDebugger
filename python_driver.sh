9f41f655d2aae562da2bbf3f9e484130e1c4595b
yurrig <yu_gershanov@hotmail.com>
2022-01-05 23:06:49 -0600

    Update

---
+++ python_driver.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$(dirname "${BASH_ARGV0:-$0}")" ; pwd -P )"
+
+ARGS=$*
+DEEP_DEBUGGER_PREFIX=--deep-debugger-
+
+while [[ -n $1 ]]; do
+    case "$1" in 
+        "${DEEP_DEBUGGER_PREFIX}nodejs-path")
+            NODEJS_PATH=$2
+            shift ;;
+    esac
+    shift
+done
+
+${NODEJS_PATH} ${SCRIPT_DIR}/python_driver.js
