9bd6ea852c6797270f110b9c5fbadb8fd133e9ea
yurrig <yu_gershanov@hotmail.com>
2022-01-13 11:40:19 -0600

    Update

--- server.sh
+++ server.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-SCRIPT_PATH="$(dirname "$0")"
-LOG="${SCRIPT_PATH}/log"
+LOG() {
+    echo "$1" >> /tmp/server.log
+}
 
 PIPE="$1"
 
-if [ "$#" eq "2" ]; then
-    printf "%s" "$2" > "${PIPE}"
+if [ $# = '2' ]; then
+    if [ ! -p "${PIPE}" ]; then
+        LOG "${PIPE} does not exist or is not a pipe, exiting"
+    else
+        LOG "Sending stop signal to ${PIPE} and exiting"
+        printf "%s" "$2" > "${PIPE}"
+    fi
     exit 0
 fi
 
 if [ ! -p "${PIPE}" ]; then
-    trap "rm -f "${PIPE}"" EXIT
+    LOG "Creating ${PIPE}"
+    trap 'rm -f ${PIPE}' EXIT
     mkfifo "${PIPE}"
 fi
 
 while true; do
-    if read line < "${PIPE}"; then
-        if [ "$line" == 'quit' ]; then
-            break
-        fi
-        printf "%s" "$line"
+    LOG "Waiting on ${PIPE}"
+    read -r line < "${PIPE}"
+    if [ "$line" = 'stopped' ]; then
+        LOG "Stop signal received, exiting"
+        break
     fi
+    LOG "Printing start|${line}|end"
+    printf "%s" "start|${line}|end"
 done
