ef0dc785d406f3d0cfc84dd16fcafb8bf7d17032
yurrig <yu_gershanov@hotmail.com>
2022-01-15 18:33:00 -0600

    Use selected Python interpreter

--- src/python.ts
+++ src/python.ts
@@ -13,8 +13,36 @@ const deepDebuggerPrefix = '--deep-debugger-';
 const deepDebuggerSessionNameSwitch = deepDebuggerPrefix + 'session-name';
 const deepDebuggerSessionCwdSwitch = deepDebuggerPrefix + 'session-cwd';
 
+const PYTHON = 'python';
+
+// https://github.com/formulahendry/vscode-code-runner/blob/master/src/utility.ts
+async function getPythonPath(): Promise<string> {
+    try {
+        const extension = vscode.extensions.getExtension("ms-python.python");
+        if (!extension) {
+            return PYTHON;
+        }
+        const usingNewInterpreterStorage = extension.packageJSON?.featureFlags?.usingNewInterpreterStorage;
+        if (usingNewInterpreterStorage) {
+            if (!extension.isActive) {
+                await extension.activate();
+            }
+            const execCommand = extension.exports.settings.getExecutionDetails ?
+                extension.exports.settings.getExecutionDetails().execCommand :
+                extension.exports.settings.getExecutionCommand();
+            return execCommand ? execCommand.join(" ") : PYTHON;
+        } else {
+            var pythonConfig = vscode.workspace.getConfiguration(PYTHON);
+            var retval = pythonConfig.get<string>("pythonPath");
+            return retval? retval : PYTHON;
+        }
+    } catch (error) {
+        return PYTHON;
+    }
+}
+
 function cloneDriver(origPythonPath: string): string {
-    var tempPath = path.join(os.tmpdir(), 'DeepDebugger', 'python');
+    var tempPath = path.join(os.tmpdir(), 'DeepDebugger', PYTHON);
     var extensionPath = getExtensionPath();
     var parcedExtDir = path.parse(extensionPath);
     var parcedPythonPath = path.parse(origPythonPath);
@@ -26,7 +54,7 @@ function cloneDriver(origPythonPath: string): string {
     }
 
     var tempDriverDir;
-    var relPath =path.relative(tempPath, origPythonPath);
+    var relPath = path.relative(tempPath, origPythonPath);
     if (!relPath.startsWith('..') && relPath !== origPythonPath) {
         tempDriverDir = origPythonPath;
     } else {
@@ -61,8 +89,8 @@ function cloneDriver(origPythonPath: string): string {
     return tempDriverPath;
 }
 
-export function makeBinConfig(cfg, wf) {
-    const DEFAULT_PYTHON_PATH = 'python';
+export async function makeBinConfig(cfg, wf) {
+    const DEFAULT_PYTHON_PATH = PYTHON;
     function notSet(pythonPath) {
         return !pythonPath || pythonPath === DEFAULT_PYTHON_PATH;
     }
@@ -71,15 +99,18 @@ export function makeBinConfig(cfg, wf) {
     if (notSet(origPythonPath)) {
         origPythonPath = cfg.pythonPath;
     }
-    const pythonSettings = vscode.workspace.getConfiguration(cfg.type);
+    const pythonSettings = vscode.workspace.getConfiguration(PYTHON);
     if (notSet(origPythonPath)) {
         origPythonPath = pythonSettings.get<string>('defaultInterpreterPath');
     }
     if (notSet(origPythonPath)) {
-        origPythonPath = pythonSettings.get<string>('python');
+        origPythonPath = pythonSettings.get<string>(PYTHON);
     }
     if (notSet(origPythonPath)) {
         origPythonPath = pythonSettings.get<string>('pythonPath');
+    }
+    if (notSet(origPythonPath)) {
+        origPythonPath = await getPythonPath();
     }
     if (notSet(origPythonPath)) {
         origPythonPath = hasbin.sync(DEFAULT_PYTHON_PATH);
