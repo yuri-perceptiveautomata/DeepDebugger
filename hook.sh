6f259b29ac8c4de9a447ebd21e3927846788a16a
yurrig <yu_gershanov@hotmail.com>
2022-02-17 23:53:46 -0600

    Update

--- hook.sh
+++ hook.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 PROCNAME=hook
-if [ -n "${DEEPDEBUGGER_LOGFILE}" ]; then LOGFILE="${DEEPDEBUGGER_LOGFILE}"; fi
+if [ -n "${DEEPDEBUGGER_LOGFILE}" ]
+then
+    LOGFILE="${DEEPDEBUGGER_LOGFILE}"
+fi
 
 lock () {
     while ! mkdir -p "$1".lock 2>/dev/null
@@ -26,14 +29,14 @@ LOG() {
 
 LOG "Hook started: $*"
 
-HOOK_QUEUE=${DEEPDEBUGGER_LAUNCHER_QUEUE}.${DEEPDEBUGGER_SESSION_ID}
+HOOK_QUEUE=${DEEPDEBUGGER_LAUNCHER_QUEUE}.$$
 PARAM_TYPE="\"type\": \"\""
 PARAM_CWD="\"cwd\": \"$(printf "%s" "${PWD}" | base64 -w 0)\""
 PARAM_CMDLINE="\"cmdline\": \"$(while [ -n "$1" ]; do printf "%s" "$1" | base64 -w 0; printf "-"; shift; done)\""
 PARAM_PARENTSESSION="\"deepDbgParentSessionID\": \"${DEEPDEBUGGER_SESSION_ID}\""
 PARAM_HOOKPIPE="\"deepDbgHookPipe\": \"${HOOK_QUEUE}\""
 LOG "Getting the environment"
-PARAM_ENV_CONT=$(printenv | while IFS= read -r line; do (printf "%s" "${line}" | base64 -w 0); printf "-"; done)
+PARAM_ENV_CONT=$(printenv | while IFS= read -r LINE; do (printf "%s" "${LINE}" | base64 -w 0); printf "-"; done)
 PARAM_ENV="\"environment\": \"${PARAM_ENV_CONT}\""
 
 PARAMS_JSON="{${PARAM_TYPE},${PARAM_CWD},${PARAM_CMDLINE},${PARAM_PARENTSESSION},${PARAM_HOOKPIPE},${PARAM_ENV}}"
@@ -44,7 +47,8 @@ printf "%s" "${PARAMS_JSON}" > "${DEEPDEBUGGER_LAUNCHER_QUEUE}"
 release_lock "${DEEPDEBUGGER_LAUNCHER_QUEUE}"
 
 lock "${HOOK_QUEUE}"
-if [ ! -p "${HOOK_QUEUE}" ]; then
+if [ ! -p "${HOOK_QUEUE}" ]
+then
     trap 'lock ${HOOK_QUEUE}; rm -f ${HOOK_QUEUE}; release_lock ${HOOK_QUEUE}' EXIT
     LOG "Creating fifo ${HOOK_QUEUE}"
     mkfifo "${HOOK_QUEUE}"
@@ -52,5 +56,5 @@ fi
 release_lock "${HOOK_QUEUE}"
 
 LOG "Waiting on ${HOOK_QUEUE}"
-IFS= read -r line < "${HOOK_QUEUE}"
-LOG "Received ${line}"
+IFS= read -r LINE < "${HOOK_QUEUE}"
+LOG "Received ${LINE}"
--- python_driver.sh
+++ python_driver.sh
@@ -68,14 +68,14 @@ if [ -z "${LAUNCH_DEBUGGER}" ]; then
     exit $?
 fi
 
-HOOK_QUEUE=${DEEPDEBUGGER_LAUNCHER_QUEUE}.${DEEPDEBUGGER_SESSION_ID}
+HOOK_QUEUE=${DEEPDEBUGGER_LAUNCHER_QUEUE}.$$
 PARAM_TYPE="\"type\": \"$(printf "%s" "deepdbg-pythonBin" | base64 -w 0)\""
 PARAM_CWD="\"cwd\": \"$(printf "%s" "${PWD}" | base64 -w 0)\""
 PARAM_CMDLINE="\"cmdline\": \"${PARAM_ARGS}\""
 PARAM_HOOKPIPE="\"deepDbgHookPipe\": \"${HOOK_QUEUE}\""
 PARAM_PROGRAM=\"program\":\"$(printf "%s" "${PYTHON_PATH}" | base64 -w 0)\"
 LOG "Getting the environment"
-PARAM_ENV_CONT=$(printenv | while IFS= read -r line; do (printf "%s" "${line}" | base64 -w 0); printf "-"; done)
+PARAM_ENV_CONT=$(printenv | while IFS= read -r LINE; do (printf "%s" "${LINE}" | base64 -w 0); printf "-"; done)
 PARAM_ENV="\"environment\": \"${PARAM_ENV_CONT}\""
 
 PARAMS_JSON="{${PARAM_TYPE},${PARAM_PROGRAM},${PARAM_CWD},${PARAM_CMDLINE},${PARAM_HOOKPIPE},${PARAM_ENV}}"
@@ -94,5 +94,5 @@ fi
 release_lock "${HOOK_QUEUE}"
 
 LOG "Waiting on ${HOOK_QUEUE}"
-IFS= read -r line < "${HOOK_QUEUE}"
-LOG "Received ${line}"
+IFS= read -r LINE < "${HOOK_QUEUE}"
+LOG "Received ${LINE}"
